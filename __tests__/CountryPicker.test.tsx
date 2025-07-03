43607a13dae339ebda116f38abbe1437d63b3edd
RFN <ryder@rgn.one>
2025-07-03 18:20:18 +0200

    Remove jest

--- __tests__/CountryPicker.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react-native"
-import { CountryPicker } from "../src/CountryPicker"
-
-it("Can be created", () => {
-	const picker = render(<CountryPicker onSelect={() => {}} />)
-	expect(picker).toBeDefined()
-})
