2df1df58d59a5d71020de911a0fd3d6fea36ed85
RFN <ryder@rgn.one>
2026-01-25 17:39:44 +0100

    Changes

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,7 @@ module.exports = {
 	],
 	testPathIgnorePatterns: ["/node_modules/", "/dist/", "/android/", "/ios/"],
 	transformIgnorePatterns: [
-		"node_modules/(?!((@expo|expo|expo-modules-core|@expo(nent)?/.*|@react-native|react-native|@react-native-community|@react-navigation|@shopify/flash-list))/)",
-	],
+"node_modules/(?!.*(@expo|expo|expo-modules-core|@expo(nent)?/.*|@react-native|react-native|@react-native-community|@react-navigation|@shopify/flash-list))",	],
 	moduleNameMapper: {
 		"react-native/Libraries/Animated/NativeAnimatedHelper": "<rootDir>/__mocks__/NativeAnimatedHelper.ts",
 	},
